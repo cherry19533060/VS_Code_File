6ad9a2415153a2cb1dfb9006862234f2c9726378
James Tsang <cherry19533060@gmail.com>
2022-11-02 20:19:07 +0800

    Check how many pass scores.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("NTUE")
-print("Welcome to TW!")
-print('2022年10月19日 20:34:38')
+my_list = [75, 32, 40, 70, 100, 80]
+count = 0
+# for item in my_list:
+#     count += 1
+#     if item >= 60:
+#         print('Student Number', count,'Congratulation! You pass the exam!')
+#     else:
+#         print('Student Number', count,"Don't worry! You try your best!")
+
+for index, item in enumerate(my_list, start=1):
+    if item >= 60:
+        print('Student Number', index,'Congratulation! You pass the exam!')
+    else:
+        print('Student Number', index,"Don't worry! You try your best!")
